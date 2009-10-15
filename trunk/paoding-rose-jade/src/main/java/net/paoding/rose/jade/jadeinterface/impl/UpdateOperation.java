86e1684caac857170afa79ef7500dc7f9ad88879
in355hz@gmail.com <in355hz@gmail.com@d6cd186a-a80a-11de-ba6e-69e41c8db662>
2009-10-15 10:50:28 +0000

    使用 Identity 返回值代替了 @SQLReturn 注释

--- trunk/paoding-rose-jade/src/main/java/net/paoding/rose/jade/jadeinterface/impl/UpdateOperation.java
+++ trunk/paoding-rose-jade/src/main/java/net/paoding/rose/jade/jadeinterface/impl/UpdateOperation.java
@@ -5,10 +5,9 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.paoding.rose.jade.jadeinterface.Identity;
 import net.paoding.rose.jade.jadeinterface.annotation.SQL;
 import net.paoding.rose.jade.jadeinterface.annotation.SQLParam;
-import net.paoding.rose.jade.jadeinterface.annotation.SQLReturn;
-import net.paoding.rose.jade.jadeinterface.annotation.SQLReturnType;
 import net.paoding.rose.jade.jadeinterface.provider.DataAccess;
 
 import org.springframework.util.NumberUtils;
@@ -49,33 +48,36 @@ public class UpdateOperation implements JdbcOperation {
         }
 
         // 检查返回值类型
-        SQLReturn sqlReturn = method.getAnnotation(SQLReturn.class);
-        if (SQLReturnType.ID == sqlReturn.value()) {
+        Class<?> returnClassType = method.getReturnType();
+
+        if (returnClassType == Identity.class) {
 
             // 执行 INSERT 查询
-            return dataAccess.insertReturnId(sqlCommand.value(), parameters);
+            Number number = dataAccess.insertReturnId(sqlCommand.value(), parameters);
 
-        } else if (SQLReturnType.UPDATE_COUNT == sqlReturn.value()) {
+            // 将结果转成方法的返回类型
+            return new Identity(number);
+
+        } else if (returnClassType != Void.class) {
 
             // 执行 UPDATE / DELETE 查询
             int updated = dataAccess.update(sqlCommand.value(), parameters);
 
             // 将结果转成方法的返回类型
-            Class<?> returnClassType = method.getReturnType();
+            if (returnClassType == boolean.class || returnClassType == Boolean.class) {
+                return updated > 0;
+            }
+
             if (returnClassType == int.class || returnClassType == Integer.class
                     || returnClassType == long.class || returnClassType == Long.class) {
                 return updated;
             }
 
             if (Number.class.isAssignableFrom(returnClassType)) {
-                return NumberUtils.parseNumber(String.valueOf(updated), returnClassType);
-            }
-
-            if (returnClassType == boolean.class || returnClassType == Boolean.class) {
-                return updated > 0;
+                return NumberUtils.convertNumberToTargetClass(updated, returnClassType);
             }
         }
 
-        return null;
+        return null; // 没有返回值
     }
 }
--- trunk/paoding-rose-jade/src/main/java/net/paoding/rose/jade/jadeinterface/provider/springjdbctemplte/SpringJdbcTemplateDataAccess.java
+++ trunk/paoding-rose-jade/src/main/java/net/paoding/rose/jade/jadeinterface/provider/springjdbctemplte/SpringJdbcTemplateDataAccess.java
@@ -1,5 +1,9 @@
 package net.paoding.rose.jade.jadeinterface.provider.springjdbctemplte;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -10,12 +14,37 @@ import javax.sql.DataSource;
 
 import net.paoding.rose.jade.jadeinterface.provider.DataAccess;
 
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.SqlProvider;
+import org.springframework.util.Assert;
 
 public class SpringJdbcTemplateDataAccess implements DataAccess {
 
-    private static final Pattern PATTERN = Pattern.compile("\\:([a-zA-Z0-9_]*)");
+    // 创建  PreparedStatement 时指定  Statement.RETURN_GENERATED_KEYS 属性
+    private static class GenerateKeysPreparedStatementCreator implements PreparedStatementCreator,
+            SqlProvider {
+
+        private final String sql;
+
+        public GenerateKeysPreparedStatementCreator(String sql) {
+            Assert.notNull(sql, "SQL must not be null");
+            this.sql = sql;
+        }
+
+        public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+            return con.prepareStatement(this.sql, Statement.RETURN_GENERATED_KEYS);
+        }
+
+        public String getSql() {
+            return this.sql;
+        }
+    }
+
+    private static final Pattern PATTERN = Pattern.compile("\\:([a-zA-Z0-9_\\.]*)");
 
     private JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
@@ -30,20 +59,14 @@ public class SpringJdbcTemplateDataAccess implements DataAccess {
             throw new IllegalArgumentException("SQL must not be null");
         }
 
-        Matcher matcher = PATTERN.matcher(sql);
+        // 用 :name 参数的值填充列表
+        final List<Object> values = new ArrayList<Object>();
 
-        if (matcher.find()) {
+        sql = processParameters(sql, parameters, values);
 
-            final List<Object> values = new ArrayList<Object>();
+        if (!values.isEmpty()) {
 
-            do {
-                final String name = matcher.group(1).trim();
-
-                values.add(parameters.get(name));
-
-            } while (matcher.find());
-
-            return jdbcTemplate.query(matcher.replaceAll("?"), values.toArray(), rowMapper);
+            return jdbcTemplate.query(sql, values.toArray(), rowMapper);
 
         } else {
 
@@ -58,20 +81,14 @@ public class SpringJdbcTemplateDataAccess implements DataAccess {
             throw new IllegalArgumentException("SQL must not be null");
         }
 
-        Matcher matcher = PATTERN.matcher(sql);
+        // 用 :name 参数的值填充列表
+        final List<Object> values = new ArrayList<Object>();
 
-        if (matcher.find()) {
+        sql = processParameters(sql, parameters, values);
 
-            final List<Object> values = new ArrayList<Object>();
+        if (!values.isEmpty()) {
 
-            do {
-                final String name = matcher.group(1).trim();
-
-                values.add(parameters.get(name));
-
-            } while (matcher.find());
-
-            return jdbcTemplate.update(matcher.replaceAll("?"), values.toArray());
+            return jdbcTemplate.update(sql, values.toArray());
 
         } else {
 
@@ -80,33 +97,77 @@ public class SpringJdbcTemplateDataAccess implements DataAccess {
     }
 
     @Override
-    public Number insertReturnId(final String sql, final Map<String, ?> parameters) {
+    public Number insertReturnId(String sql, Map<String, ?> parameters) {
 
         if (sql == null) {
             throw new IllegalArgumentException("SQL must not be null");
         }
 
-        Matcher matcher = PATTERN.matcher(sql);
+        // 用 :name 参数的值填充列表
+        final List<Object> values = new ArrayList<Object>();
 
-        if (matcher.find()) {
+        sql = processParameters(sql, parameters, values);
 
-            final List<Object> values = new ArrayList<Object>();
-
-            do {
-                final String name = matcher.group(1).trim();
-
-                values.add(parameters.get(name));
-
-            } while (matcher.find());
+        if (!values.isEmpty()) {
 
             PreparedStatementCallbackReturnId callbackReturnId = new PreparedStatementCallbackReturnId(
                     new ArgPreparedStatementSetter(values.toArray()));
-            return (Number) jdbcTemplate.execute(matcher.replaceAll("?"), callbackReturnId);
+            return (Number) jdbcTemplate.execute(new GenerateKeysPreparedStatementCreator(sql),
+                    callbackReturnId);
 
         } else {
 
             PreparedStatementCallbackReturnId callbackReturnId = new PreparedStatementCallbackReturnId();
-            return (Number) jdbcTemplate.execute(sql, callbackReturnId);
+            return (Number) jdbcTemplate.execute(new GenerateKeysPreparedStatementCreator(sql),
+                    callbackReturnId);
         }
+    }
+
+    /**
+     * 查找 SQL 语句中所有的 :name, :name.property 参数, 将值写入列表, 并且返回包含 '?' 的 SQL 语句。
+     * 
+     * @param sql - SQL 语句
+     * @param parameters - [in] 传入的参数
+     * @param values - [out] 填充的参数列表
+     * 
+     * @return 包含 '?' 的 SQL 语句
+     */
+    protected String processParameters(String sql, Map<String, ?> parameters,
+            final List<Object> values) {
+
+        // 匹配符合  :name 格式的参数
+        Matcher matcher = PATTERN.matcher(sql);
+        if (matcher.find()) {
+
+            do {
+                // 提取参数名称
+                final String name = matcher.group(1).trim();
+
+                Object value = null;
+
+                // 解析  a.b.c 类型的名称 
+                int index = name.indexOf('.');
+                if (index >= 0) {
+
+                    // 用  BeanWrapper 获取属性值
+                    Object bean = parameters.get(name.substring(0, index));
+                    if (bean != null) {
+                        BeanWrapper beanWrapper = new BeanWrapperImpl(bean);
+                        value = beanWrapper.getPropertyValue(name.substring(index + 1));
+                    }
+
+                } else {
+                    // 直接获取值
+                    value = parameters.get(name);
+                }
+
+                values.add(value);
+
+            } while (matcher.find());
+
+            return matcher.replaceAll("?");
+        }
+
+        return sql;
     }
 }
