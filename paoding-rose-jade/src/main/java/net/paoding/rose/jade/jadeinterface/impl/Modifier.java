710816be05e4353f12cb2074e2efadb032c515a3
qieqie.wang <qieqie.wang@d6cd186a-a80a-11de-ba6e-69e41c8db662>
2009-10-19 03:04:25 +0000

    提交modifier类

---
+++ paoding-rose-jade/src/main/java/net/paoding/rose/jade/jadeinterface/impl/Modifier.java
@@ -0,0 +1,27 @@
+package net.paoding.rose.jade.jadeinterface.impl;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+/**
+ * 
+ * 
+ * @author 王志亮 [qieqie.wang@gmail.com]
+ */
+public class Modifier {
+
+    private Method method;
+
+    public String getMethodName() {
+        return method.getName();
+    }
+
+    public Class<?> getReturnType() {
+        return method.getReturnType();
+    }
+
+    public <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
+        return (T) method.getAnnotation(annotationClass);
+    }
+
+}
