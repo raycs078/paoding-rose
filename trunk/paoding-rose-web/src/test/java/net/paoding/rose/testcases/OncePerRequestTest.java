d6961b6e513603244e69f1060da3d13e20c6257c
qieqie.wang <qieqie.wang@d6cd186a-a80a-11de-ba6e-69e41c8db662>
2009-11-24 16:07:42 +0000

    add usecases

---
+++ trunk/paoding-rose-web/src/test/java/net/paoding/rose/testcases/OncePerRequestTest.java
@@ -0,0 +1,14 @@
+package net.paoding.rose.testcases;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+
+public class OncePerRequestTest extends AbstractControllerTest {
+
+    public void test() throws ServletException, IOException {
+//        assertEquals("ok", invoke("/oncePerRequest"));
+    }
+
+}
