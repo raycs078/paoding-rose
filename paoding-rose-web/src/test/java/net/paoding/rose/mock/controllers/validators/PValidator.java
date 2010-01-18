3d2015576623fd2d2523190fc5717cd4e899b3d8
qieqie.wang <qieqie.wang@d6cd186a-a80a-11de-ba6e-69e41c8db662>
2010-01-18 03:52:21 +0000

    ParamValidator.validate方法增加ParamMetaData参数

--- paoding-rose-web/src/test/java/net/paoding/rose/mock/controllers/validators/PValidator.java
+++ paoding-rose-web/src/test/java/net/paoding/rose/mock/controllers/validators/PValidator.java
@@ -1,11 +1,10 @@
 package net.paoding.rose.mock.controllers.validators;
 
-import org.springframework.validation.Errors;
-
 import net.paoding.rose.web.Invocation;
 import net.paoding.rose.web.ParamValidator;
 import net.paoding.rose.web.paramresolver.ParamMetaData;
 
+import org.springframework.validation.Errors;
 
 public class PValidator implements ParamValidator {
 
@@ -15,8 +14,8 @@ public class PValidator implements ParamValidator {
     }
 
     @Override
-    public Object validate(Invocation inv, Object target, Errors errors) {
-        return ((Integer) target) == 0 ?  "error" : true;
+    public Object validate(ParamMetaData metaData, Invocation inv, Object target, Errors errors) {
+        return ((Integer) target) == 0 ? "error" : true;
     }
 
 }
