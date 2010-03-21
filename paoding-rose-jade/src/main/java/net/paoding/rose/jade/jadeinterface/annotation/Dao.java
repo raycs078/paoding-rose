42b96ac9f797544f02d2a3ef62e15cafad2bb58e
qieqie.wang <qieqie.wang@d6cd186a-a80a-11de-ba6e-69e41c8db662>
2010-03-21 04:01:06 +0000

    增加对Dao、SQL、SQLParam、SQLType的Java Doc注释

--- paoding-rose-jade/src/main/java/net/paoding/rose/jade/jadeinterface/annotation/Dao.java
+++ paoding-rose-jade/src/main/java/net/paoding/rose/jade/jadeinterface/annotation/Dao.java
@@ -1,5 +1,17 @@
 /*
- * OPI, all rights reserved.
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License i distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package net.paoding.rose.jade.jadeinterface.annotation;
 
@@ -10,14 +22,23 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 使用 xiaonei-commons-jade 的 DAO 接口需要明确标注 {@link Dao} 注解，并指定所使用的数据源
- * (catalog)。
+ * 
+ * 用此{@link Dao}注解标注在一个符合Jade编写规范的DAO接口类上，明确标注这是Jade DAO接口。
  * 
  * <p>
- * xiaonei-commons-jade 要求数据访问对象接口必须在 xxx.yyy.dao 包或其子 package 包下 (xxx.yyy
- * 命名可任意)， 并且以 "Dao" 为结尾。
+ * 被Jade识别的DAO接口必须符合3个原则：<br>
+ * <ul>
+ * <li>DAO接口类声明在dao包下，即yourcampany.yourapp.dao的package下或子package下；</li>
+ * <li>DAO接口名称必须以DAO或Dao结尾；</li>
+ * <li>DAO接口必须标注{@link Dao}注解；</li>
+ * </ul>
+ * <p>
+ * 
+ * 如果DAO接口被打包成为一个jar的，为了要被Jade识别，必须在这个jar的MANIFEST.MF文件中包含"Rose: DAO"的标识(
+ * 标识Rose: *亦可以，但建议开发者们精确标志DAO而非*，这有助于提高启动速度)。
  * 
  * @author 王志亮 [qieqie.wang@gmail.com]
+ * @author 廖涵 [in355hz@gmail.com]
  */
 @Target( { ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
@@ -25,7 +46,7 @@ import java.lang.annotation.Target;
 public @interface Dao {
 
     /**
-     * 指定此所标注的 Dao 类所要使用的数据源。
+     * 指定此所标注的 DAO 所要使用的数据源名称。
      * 
      * @return 使用的数据源
      */
--- paoding-rose-jade/src/main/java/net/paoding/rose/jade/jadeinterface/annotation/SQLParam.java
+++ paoding-rose-jade/src/main/java/net/paoding/rose/jade/jadeinterface/annotation/SQLParam.java
@@ -1,5 +1,17 @@
 /*
- * OPI, all rights reserved.
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License i distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package net.paoding.rose.jade.jadeinterface.annotation;
 
@@ -10,9 +22,18 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 把 {@link SQLParam} 标注在 SQL 查询的方法参数上，说明该参数是 SQL 语句中某参数的值。
+ * 用{@link SQLParam} 注解标注DAO方法的参数，指定参数的名称，使得可以在SQL中通过":参数名"的方式使用它。
+ * Jade通过PreparedStatment动态地把参数值提交给数据库执行。
+ * <p>
+ * 
+ * <span style='margin-left:50px;'>
+ * <code>@SQL("SELECT id, account, name FROM user WHERE id=:userId")<span>
+ * <br>
+ * <span style='margin-left:50px;'> public User getUser(@SQLParam("userId") String id);</code><span>
+ * <p>
  * 
  * @author 王志亮 [qieqie.wang@gmail.com]
+ * @author 廖涵 [in355hz@gmail.com]
  */
 @Target( { ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
