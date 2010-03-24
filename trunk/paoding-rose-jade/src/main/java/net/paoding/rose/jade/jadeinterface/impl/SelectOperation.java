020030dfefd07640f0f81e91ea7ab14db2eaf5ea
qieqie.wang <qieqie.wang@d6cd186a-a80a-11de-ba6e-69e41c8db662>
2010-03-24 03:59:33 +0000

    "使DAO支持$1、$2.name等SQL写法"改为"使DAO支持:1、:2.name等SQL"

--- trunk/paoding-rose-jade/src/main/java/net/paoding/rose/jade/jadeinterface/impl/SelectOperation.java
+++ trunk/paoding-rose-jade/src/main/java/net/paoding/rose/jade/jadeinterface/impl/SelectOperation.java
@@ -66,7 +66,7 @@ public class SelectOperation implements JdbcOperation {
         } else {
             parameters = new HashMap<String, Object>(args.length * 2);
             for (int i = 0; i < args.length; i++) {
-                parameters.put("$" + (i + 1), args[i]);
+                parameters.put(":" + (i + 1), args[i]);
             }
             for (int i = 0; i < annotations.length; i++) {
                 SQLParam annotation = annotations[i];
