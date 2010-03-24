be9ec065c8e857e61a9cf663dfc9f11e3ac5cf73
qieqie.wang <qieqie.wang@d6cd186a-a80a-11de-ba6e-69e41c8db662>
2010-03-24 10:02:20 +0000

    --by 王志亮和廖涵

---
+++ paoding-rose-jade/src/main/java/net/paoding/rose/jade/jadeinterface/provider/jdbctemplate/exql/ExqlDataAccess.java
@@ -0,0 +1,80 @@
+package net.paoding.rose.jade.jadeinterface.provider.jdbctemplate.exql;
+
+import java.sql.SQLSyntaxErrorException;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import net.paoding.rose.jade.jadeinterface.exql.ExqlContext;
+import net.paoding.rose.jade.jadeinterface.exql.ExqlPattern;
+import net.paoding.rose.jade.jadeinterface.exql.impl.ExqlContextImpl;
+import net.paoding.rose.jade.jadeinterface.exql.impl.ExqlPatternImpl;
+import net.paoding.rose.jade.jadeinterface.provider.DataAccess;
+import net.paoding.rose.jade.jadeinterface.provider.Modifier;
+import net.paoding.rose.jade.jadeinterface.provider.jdbctemplate.JdbcTemplateDataAccess;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.jdbc.core.RowMapper;
+
+/**
+ * 提供动态: SQL 语句功能的 {@link DataAccess} 实现。
+ * 
+ * @author han.liao
+ */
+public class ExqlDataAccess extends JdbcTemplateDataAccess {
+
+    public ExqlDataAccess() {
+        super();
+    }
+
+    public ExqlDataAccess(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public List<?> select(String jdQL, Modifier modifier, Map<String, ?> parameters,
+            RowMapper rowMapper) {
+
+        ExqlContext context = processConversion(jdQL, modifier, parameters);
+        return selectByJdbcTemplate(context.flushOut(), context.getParams(), rowMapper);
+    }
+
+    @Override
+    public int update(String jdQL, Modifier modifier, Map<String, ?> parameters) {
+
+        ExqlContext context = processConversion(jdQL, modifier, parameters);
+        return updateByJdbcTemplate(context.flushOut(), context.getParams());
+    }
+
+    @Override
+    public Number insertReturnId(String jdQL, Modifier modifier, Map<String, ?> parameters) {
+
+        ExqlContext context = processConversion(jdQL, modifier, parameters);
+        return insertReturnIdByJdbcTemplate(context.flushOut(), context.getParams());
+    }
+
+    // 转换   JDQL 语句为正常的  SQL 语句
+    protected ExqlContext processConversion(String jdQL, Modifier modifier,
+            Map<String, ?> parameters) throws DataAccessException {
+
+        if (jdQL == null) {
+            throw new IllegalArgumentException("SQL must not be null");
+        }
+
+        // 转换语句中的表达式
+        ExqlPattern pattern = ExqlPatternImpl.compile(jdQL);
+        ExqlContext context = new ExqlContextImpl();
+
+        try {
+            pattern.execute(context, parameters, modifier.getDefinition().getConstants());
+
+        } catch (Exception e) {
+            throw new BadSqlGrammarException("ExqlPattern.execute", jdQL,
+                    new SQLSyntaxErrorException("Error executing pattern", e));
+        }
+
+        return context;
+    }
+}
