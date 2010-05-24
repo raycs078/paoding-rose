90cbc0ac76f8d19a1dc4985b40988dbb36732eef
weibo.leo <weibo.leo@d6cd186a-a80a-11de-ba6e-69e41c8db662>
2010-05-24 03:35:38 +0000

    isAccepted支持多条件判断所对应的测试用例

--- paoding-rose-web/src/test/java/net/paoding/rose/testcases/controllers/IfControllerTest.java
+++ paoding-rose-web/src/test/java/net/paoding/rose/testcases/controllers/IfControllerTest.java
@@ -8,6 +8,45 @@ import net.paoding.rose.testcases.AbstractControllerTest;
 
 public class IfControllerTest extends AbstractControllerTest {
 
+	public void testIfab1() throws ServletException, IOException {
+    	request.addParameter("a", "a");
+    	request.addParameter("b", "b");
+    	assertEquals("ab1", invoke("/if"));
+    }
+	
+	public void testIfab2() throws ServletException, IOException {
+    	request.addParameter("a", "111");
+    	request.addParameter("b", "222");
+    	assertEquals("ab2", invoke("/if"));
+    }
+	
+	public void testIfab3() throws ServletException, IOException {
+    	request.addParameter("a", "fdsaffas");
+    	request.addParameter("b", "fasdfas");
+    	assertEquals("ab3", invoke("/if"));
+    }
+	
+	public void testIfNotAb1() throws ServletException, IOException {
+    	request.addParameter("a", "a");
+    	request.addParameter("b", "bfsdfsd");
+    	System.out.println(invoke("/if"));
+    	assertEquals("ab3", invoke("/if"));
+    }
+	
+	public void testIfabc1() throws ServletException, IOException {
+    	request.addParameter("a", "a");
+    	request.addParameter("b", "b");
+    	request.addParameter("c", "c");
+    	assertEquals("abc1", invoke("/if"));
+    }
+	
+	public void testIfNotAbc1() throws ServletException, IOException {
+    	request.addParameter("a", "a");
+    	request.addParameter("b", "b");
+    	request.addParameter("c", "fasdfsad");
+    	assertEquals("ab1", invoke("/if"));
+    }
+	
     public void testNotIf() throws ServletException, IOException {
         assertEquals("a", invoke("/if"));
     }
@@ -35,4 +74,5 @@ public class IfControllerTest extends AbstractControllerTest {
     public void testSubDir() throws ServletException, IOException {
         assertEquals("d", invoke("/if/d"));
     }
+    
 }
