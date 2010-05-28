201e0db81b4516a56e965d5d7b4791ed419d2d86
cn.wang.tai <cn.wang.tai@d6cd186a-a80a-11de-ba6e-69e41c8db662>
2010-05-28 08:49:46 +0000

    添加注释,修改接口

--- paoding-rose-jade/src/main/java/net/paoding/rose/jade/plugin/AbsJadePlugin.java
+++ paoding-rose-jade/src/main/java/net/paoding/rose/jade/plugin/AbsJadePlugin.java
@@ -1,8 +1,22 @@
+/*
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License i distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.paoding.rose.jade.plugin;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -20,7 +34,7 @@ public abstract class AbsJadePlugin implements IJadePlugin {
 
     @Override
     public void start(DataSource dataSource, String sqlString, Modifier modifier,
-            Map<String, Object> parameters) {
+            Object[] arrayParameters) {
         this.data.set(new DataModel());
         DataModel d = data.get();
 
@@ -31,7 +45,7 @@ public abstract class AbsJadePlugin implements IJadePlugin {
             d.setClientIp("");
             e.printStackTrace();
         }
-        startPlugin(d, dataSource, sqlString, modifier, parameters);
+        startPlugin(d, dataSource, sqlString, modifier, arrayParameters);
     }
 
     @Override
@@ -66,7 +80,7 @@ public abstract class AbsJadePlugin implements IJadePlugin {
      * @param data
      */
     protected abstract void startPlugin(DataModel data, DataSource dataSource, String sqlString,
-            Modifier modifier, Map<String, Object> parameters);
+            Modifier modifier, Object[] arrayParameters);
 
     /**
      * endPlugin<br>
