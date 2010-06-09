70e2400c053410cfa691e50c9dd8b5a34b56bf26
qieqie.wang <qieqie.wang@d6cd186a-a80a-11de-ba6e-69e41c8db662>
2010-06-09 06:35:12 +0000

    增加try-catch等为调试而产生的微少代码

--- paoding-rose-scanning/src/main/java/net/paoding/rose/scanning/vfs/FileSystemManager.java
+++ paoding-rose-scanning/src/main/java/net/paoding/rose/scanning/vfs/FileSystemManager.java
@@ -59,50 +59,55 @@ public class FileSystemManager {
     }
 
     public synchronized FileObject resolveFile(URL url) throws IOException {
-        if (traceEnabled) {
-            logger.trace("[fs] resolveFile ... by url '" + url + "'");
-        }
-        String urlString = url.toString();
-        FileObject object = cached.get(urlString);
-
-        if (object != null) {
+        try {
             if (traceEnabled) {
-                logger.trace("[fs] found cached file for url '" + urlString + "'");
+                logger.trace("[fs] resolveFile ... by url '" + url + "'");
+            }
+            String urlString = url.toString();
+            FileObject object = cached.get(urlString);
+
+            if (object != null) {
+                if (traceEnabled) {
+                    logger.trace("[fs] found cached file for url '" + urlString + "'");
+                }
+                return object;
+            }
+            if (ResourceUtils.isJarURL(url)) {
+                if (!urlString.endsWith("/")) {
+                    object = resolveFile(urlString + "/");
+                }
+                if (object == null || !object.exists()) {
+                    object = new JarFileObject(this, url);
+                    if (traceEnabled) {
+                        logger.trace("[fs] create jarFileObject for '" + urlString + "'");
+                    }
+                }
+            } else {
+                File file = ResourceUtils.getFile(url);
+                if (file.isDirectory()) {
+                    if (!urlString.endsWith("/")) {
+                        urlString = urlString + "/";
+                        url = new URL(urlString);
+                    }
+                } else if (file.isFile()) {
+                    if (urlString.endsWith("/")) {
+                        urlString = StringUtils.removeEnd(urlString, "/");
+                        url = new URL(urlString);
+                    }
+                }
+                object = new SimpleFileObject(this, url);
+                if (traceEnabled) {
+                    logger.trace("[fs] create simpleFileObject for '" + urlString + "'");
+                }
+            }
+            if (object.exists()) {
+                cached.put(urlString, object);
             }
             return object;
+        } catch (IOException e) {
+            logger.error(e.getMessage() + ":" + url, e);
+            throw e;
         }
-        if (ResourceUtils.isJarURL(url)) {
-            if (!urlString.endsWith("/")) {
-                object = resolveFile(urlString + "/");
-            }
-            if (object == null || !object.exists()) {
-                object = new JarFileObject(this, url);
-                if (traceEnabled) {
-                    logger.trace("[fs] create jarFileObject for '" + urlString + "'");
-                }
-            }
-        } else {
-            File file = ResourceUtils.getFile(url);
-            if (file.isDirectory()) {
-                if (!urlString.endsWith("/")) {
-                    urlString = urlString + "/";
-                    url = new URL(urlString);
-                }
-            } else if (file.isFile()) {
-                if (urlString.endsWith("/")) {
-                    urlString = StringUtils.removeEnd(urlString, "/");
-                    url = new URL(urlString);
-                }
-            }
-            object = new SimpleFileObject(this, url);
-            if (traceEnabled) {
-                logger.trace("[fs] create simpleFileObject for '" + urlString + "'");
-            }
-        }
-        if (object.exists()) {
-            cached.put(urlString, object);
-        }
-        return object;
     }
 
     public synchronized void clearCache() {
