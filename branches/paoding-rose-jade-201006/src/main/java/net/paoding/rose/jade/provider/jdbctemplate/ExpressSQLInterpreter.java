2fbf59373dc9f2222e0de8f49a65d0b4715fe640
qieqie.wang <qieqie.wang@d6cd186a-a80a-11de-ba6e-69e41c8db662>
2010-06-11 04:06:51 +0000

    因为wangtai在开发监控的功能，所以特分支一个没有监控功能的jade

---
+++ branches/paoding-rose-jade-201006/src/main/java/net/paoding/rose/jade/provider/jdbctemplate/ExpressSQLInterpreter.java
@@ -0,0 +1,46 @@
+package net.paoding.rose.jade.provider.jdbctemplate;
+
+import java.sql.SQLSyntaxErrorException;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import net.paoding.rose.jade.exql.ExqlPattern;
+import net.paoding.rose.jade.exql.impl.ExqlContextImpl;
+import net.paoding.rose.jade.exql.impl.ExqlPatternImpl;
+import net.paoding.rose.jade.provider.DataAccess;
+import net.paoding.rose.jade.provider.SQLInterpreterResult;
+import net.paoding.rose.jade.provider.SQLInterpreter;
+import net.paoding.rose.jade.provider.Modifier;
+
+import org.springframework.jdbc.BadSqlGrammarException;
+
+/**
+ * 提供动态: SQL 语句功能的 {@link DataAccess} 实现。
+ * 
+ * @author han.liao
+ */
+public class ExpressSQLInterpreter implements SQLInterpreter {
+
+    @Override
+    // 转换   JDQL 语句为正常的  SQL 语句
+    public SQLInterpreterResult interpret(DataSource dataSource, String sql, Modifier modifier,
+            Map<String, Object> parametersAsMap, Object[] parametersAsArray) {
+
+        // 转换语句中的表达式
+        ExqlPattern pattern = ExqlPatternImpl.compile(sql);
+        ExqlContextImpl context = new ExqlContextImpl(sql.length() + 32);
+
+        try {
+            pattern.execute(context, parametersAsMap, modifier.getDefinition().getConstants());
+
+        } catch (Exception e) {
+            String daoInfo = modifier.toString();
+            throw new BadSqlGrammarException(daoInfo, sql, new SQLSyntaxErrorException(daoInfo
+                    + " @SQL('" + sql + "')", e));
+        }
+
+        return context;
+    }
+
+}
