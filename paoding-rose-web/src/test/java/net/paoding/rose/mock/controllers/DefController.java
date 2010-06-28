caad86980860336d616e2c1762f6c7d0661dc35a
qieqie.wang <qieqie.wang@d6cd186a-a80a-11de-ba6e-69e41c8db662>
2010-06-28 06:30:11 +0000

    对前几天提交的修改带出的bug做fix: {xxx}.html的404得到解决

--- paoding-rose-web/src/test/java/net/paoding/rose/mock/controllers/DefController.java
+++ paoding-rose-web/src/test/java/net/paoding/rose/mock/controllers/DefController.java
@@ -1,15 +1,24 @@
 package net.paoding.rose.mock.controllers;
 
+import net.paoding.rose.web.Invocation;
 import net.paoding.rose.web.annotation.Param;
 import net.paoding.rose.web.annotation.Path;
 import net.paoding.rose.web.annotation.rest.Get;
 
-@Path({"/", "def" })
+@Path( { "/", "def" })
 public class DefController {
 
     @Get("/")
     public String index() {
         return "index";
+    }
+
+    /**
+     * 支持特殊名称，如 /xxxx.html
+     */
+    @Get("t/{tinyurl:[0-9a-zA-Z]+}.html")
+    public String tinyurl(Invocation inv, @Param("tinyurl") String tinyurl) {
+        return tinyurl;
     }
 
     public String method() {
