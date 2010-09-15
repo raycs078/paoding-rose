7d78c230aeb03965e268cc1fe8b02c0dea9fb9a7
qieqie.wang <qieqie.wang@d6cd186a-a80a-11de-ba6e-69e41c8db662>
2010-09-15 08:31:00 +0000

    使PUT、DELETE over POST能够通过hidden完成

--- trunk/paoding-rose-web/src/main/java/net/paoding/rose/web/RequestPath.java
+++ trunk/paoding-rose-web/src/main/java/net/paoding/rose/web/RequestPath.java
@@ -51,6 +51,8 @@ public class RequestPath {
 
     private Dispatcher dispatcher;
 
+    private HttpServletRequest request;
+
     public RequestPath(ReqMethod method, String uri, String ctxpath, Dispatcher dispatcher) {
         this.setMethod(method);
         setUri(uri);
@@ -60,8 +62,9 @@ public class RequestPath {
     }
 
     public RequestPath(HttpServletRequest request) {
+        this.request = request;
         // method
-        setMethod(parseMethod(request));
+        // setMethod(parseMethod(request));
 
         // ctxpath
         setCtxpath(request.getContextPath());
@@ -116,25 +119,27 @@ public class RequestPath {
     private ReqMethod parseMethod(HttpServletRequest request) {
         ReqMethod reqMethod = ReqMethod.parse(request.getMethod());
         if (reqMethod != null && reqMethod.equals(ReqMethod.POST)) {
-            // 为什么不用getParameter：
-            // 1、使_method只能在queryString中，不能在body中
-            // 2、getParameter会导致encoding，使用UTF-8? 尽量不做这个假设
-            String queryString = request.getQueryString();
-            if (queryString != null) {
-                int start = queryString.indexOf("_method=");
-                if (start >= 0) {
-                    if (start == 0 || queryString.charAt(start - 1) == '&') {
-                        int end = queryString.indexOf('&', start);
-                        String method = queryString.substring(start + "_method=".length(),//
-                                end > 0 ? end : queryString.length());
-                        ReqMethod _reqMethod = ReqMethod.parse(method);
-                        if (_reqMethod != null) {
-                            if (logger.isDebugEnabled()) {
-                                logger.debug("override http method from POST to " + _reqMethod);
-                            }
-                            reqMethod = _reqMethod;
-                        }
+            //
+            if (request.getCharacterEncoding() == null) {
+                try {
+                    request.setCharacterEncoding("UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    throw new Error(e);
+                }
+                if (logger.isDebugEnabled()) {
+                    logger.debug("set request.characterEncoding by default(requestPath):"
+                            + request.getCharacterEncoding());
+                }
+            }
+            //
+            String method = request.getParameter("_method");
+            if (method != null) {
+                ReqMethod _reqMethod = ReqMethod.parse(method);
+                if (_reqMethod != null) {
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("override http method from POST to " + _reqMethod);
                     }
+                    reqMethod = _reqMethod;
                 }
             }
         }
@@ -158,6 +163,9 @@ public class RequestPath {
     }
 
     public ReqMethod getMethod() {
+        if (method == null) {
+            method = parseMethod(request);
+        }
         return method;
     }
 
